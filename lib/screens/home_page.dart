1d3087f22f58f8b5bee5daf4d3e0fc150b6ab8bd
nick <nicolas_7979@yahoo.ca>
2020-04-23 20:13:12 -0400

    - Restyle Home Page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,10 +12,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool screenSizeSet = false;
+
   @override
   Widget build(BuildContext context) {
-    screenHeight = MediaQuery.of(context).size.height;
-    screenWidth = MediaQuery.of(context).size.width;
+    if (screenSizeSet == false) {
+      screenHeight = MediaQuery.of(context).size.height;
+      screenWidth = MediaQuery.of(context).size.width;
+      screenSizeSet = true;
+    }
 
     return Scaffold(
       body: Stack(
@@ -32,183 +37,211 @@ class _HomePageState extends State<HomePage> {
           ),
           Column(
             children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 20.0),
-                height: screenHeight / 4,
-                constraints: BoxConstraints(minWidth: screenWidth),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Colors.purpleAccent, Colors.tealAccent],
-                  ),
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 3,
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 5.0),
-                        child: Image(
-                          fit: BoxFit.cover,
-                          image: AssetImage('assets/images/treble_clef.png'),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 10,
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: RichText(
-                          text: TextSpan(
-                            style: TextStyle(
-                              fontSize: 50.0,
-                              fontWeight: FontWeight.w300,
-                            ),
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: 'Piano',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                              TextSpan(
-                                text: 'scope',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+              oldHeader(),
+              songTitle(),
+              playButton(),
+              expandedContainer(1),
+              newLessonButton(),
+              expandedContainer(2),
+              footer(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget header() {
+    return Expanded(
+      flex: 4,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/pianoscope_header.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+      ),
+    );
+  }
+
+  Widget oldHeader() {
+    return Container(
+      padding: EdgeInsets.only(top: 20.0),
+      height: screenHeight / 4,
+      constraints: BoxConstraints(minWidth: screenWidth),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.greenAccent, Colors.lightBlueAccent],
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 5.0),
+              child: Image(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/treble_clef.png'),
               ),
-              Expanded(
-                flex: 4,
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(3.0, 2.0, 3.0, 10.0),
-                    child: Text(
-                      'Twinkle Twinkle Little Star',
-                      textAlign: TextAlign.center,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Expanded(
+            flex: 10,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w300,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Piano',
                       style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'scope',
+                      style: TextStyle(
                         color: Colors.white,
                       ),
                     ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: RaisedButton(
-                    elevation: 20.0,
-                    shape: const CircleBorder(),
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => LessonSelection()));
-                    },
-                    padding: EdgeInsets.zero,
-                    color: Colors.transparent,
-                    textColor: Colors.white,
-                    child: Container(
-                      height: screenWidth / 6,
-                      width: screenWidth / 6,
-                      padding: const EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                            Colors.purpleAccent,
-                            Colors.tealAccent,
-                          ],
-                        ),
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.music_note,
-                          size: screenWidth / 10,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(),
-              ),
-              Container(
-                height: screenHeight / 8,
-                child: RaisedButton(
-                  elevation: 20.0,
-                  onPressed: () {},
-                  padding: const EdgeInsets.all(0.0),
-                  color: Colors.transparent,
-                  textColor: Colors.white,
-                  child: Container(
-                    height: screenHeight / 8,
-                    width: screenWidth / 2.25,
-                    padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Colors.purpleAccent,
-                          Colors.tealAccent,
-                        ],
-                      ),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Center(
-                      child: const Text(
-                        'Play',
-                        style: TextStyle(fontSize: 40),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Container(),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.settings),
-                      iconSize: screenWidth / 9,
-                      color: Colors.black,
-                      padding: EdgeInsets.all(15.0),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.help),
-                      iconSize: screenWidth / 9,
-                      color: Colors.black,
-                      padding: EdgeInsets.all(15.0),
-                    ),
                   ],
                 ),
               ),
-            ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget songTitle() {
+    return Expanded(
+      flex: 6,
+      child: Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(3.0, 2.0, 3.0, 10.0),
+          child: Text(
+            'Canon in D - Johann Pachelbel',
+            textAlign: TextAlign.center,
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget playButton() {
+    return Flexible(
+      flex: 3,
+      child: Container(
+        height: screenHeight / 6,
+        child: RaisedButton(
+          onPressed: () {},
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          elevation: 20.0,
+          padding: EdgeInsets.zero,
+          color: Colors.transparent,
+          textColor: Colors.white,
+          child: Container(
+            height: screenHeight / 6,
+            width: screenWidth / 2,
+            padding: EdgeInsets.zero,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.greenAccent, Colors.lightBlueAccent],
+              ),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Center(
+              child: const Text(
+                'Play',
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget newLessonButton() {
+    return Flexible(
+      flex: 2,
+      child: RaisedButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => LessonSelection()));
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        elevation: 10.0,
+        padding: EdgeInsets.zero,
+        color: Colors.transparent,
+        textColor: Colors.white,
+        child: Container(
+          height: screenHeight / 12,
+          width: screenWidth / 1.75,
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.greenAccent, Colors.lightBlueAccent],
+            ),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Center(
+            child: Text(
+              'New Lesson',
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w400),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget footer() {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+            iconSize: screenWidth / 9,
+            color: Colors.black,
+            padding: EdgeInsets.all(15.0),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.help),
+            iconSize: screenWidth / 9,
+            color: Colors.black,
+            padding: EdgeInsets.all(15.0),
           ),
         ],
       ),
